e99a2439418a1ce1ce93cd9606773d9b354d5314
alscjf1329 <alscjf1329@gmail.com>
2024-05-03 21:52:28 +0900

    feat) constants 정보들 추가

---
+++ src/Constants/HomeConstants.js
@@ -0,0 +1,13 @@
+const Ranking = {
+    name: "실시간 TOP3 랭킹",
+};
+
+const Announcement = {
+    name: "공지사항",
+};
+
+const banner = {
+    url: "https://despbukkit.s3.ap-northeast-2.amazonaws.com/Banner.png",
+};
+
+export { Ranking, Announcement, banner };
